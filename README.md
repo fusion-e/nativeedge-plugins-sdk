f5539f34c207f3df4294bc94e8cf3c32121069af
Denis Pauk <pauk.denis@gmail.com>
2018-12-12 11:40:40 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+[![Build Status](https://circleci.com/gh/cloudify-incubator/cloudify-utilities-plugins-sdk.svg?style=shield&circle-token=:circle-token)](https://circleci.com/gh/cloudify-incubator/cloudify-utilities-plugins-sdk)
+
+# Cloudify Utilities SDK
+
+Utilities SDK for extending Cloudify features.
+
+
+## Contents:
+
+
+## Versions:
+
+Look to [ChangeLog](CHANGELOG.txt).
---
+++ setup.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2017-2018 Cloudify Platform Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import setuptools
+
+setuptools.setup(
+    name='cloudify-utilities-plugin',
+    version='0.0.1',
+    author='Gigaspaces.com',
+    author_email='hello@getcloudify.org',
+    description='Utilities SDK for extending Cloudify',
+    packages=['cloudify_rest_sdk',
+              'cloudify_terminal_sdk'],
+    license='LICENSE',
+    install_requires=[
+        'paramiko',  # terminal
+        "Jinja2>=2.7.2",  # terminal
+        'pyyaml',  # cloudinit and rest
+        'xmltodict']  # rest
+)
