f1f289ec64befa3059e5c135e87812adfb66f4ae
Denis Pauk <pauk.denis@gmail.com>
2018-12-27 11:49:36 +0200

    Reorganize connection code for reuse netconf/terminal ssh connection

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import setuptools
 
 setuptools.setup(
     name='cloudify-utilities-plugins-sdk',
-    version='0.0.1',
+    version='0.0.2',
     author='Cloudify Platform Ltd.',
     author_email='hello@cloudify.co',
     description='Utilities SDK for extending Cloudify',
