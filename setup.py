6851841c6bddcd5beef7d40fe074d40215d5a60a
Denis Pauk <pauk.denis@gmail.com>
2019-01-11 16:39:40 +0200

    Add prerender and raw payload

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import setuptools
 
 setuptools.setup(
     name='cloudify-utilities-plugins-sdk',
-    version='0.0.3',
+    version='0.0.4',
     author='Cloudify Platform Ltd.',
     author_email='hello@cloudify.co',
     description='Utilities SDK for extending Cloudify',
