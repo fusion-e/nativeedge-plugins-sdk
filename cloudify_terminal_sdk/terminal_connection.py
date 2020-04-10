50d174392f42a6b3bf5c0ed042c90bfb50329836
Denis Pauk <pauk.denis@gmail.com>
2020-04-10 16:49:44 +0300

    More robust 'find_any_in' in terminal connection

--- cloudify_terminal_sdk/terminal_connection.py
+++ cloudify_terminal_sdk/terminal_connection.py
@@ -40,9 +40,11 @@ class TextConnection(base_connection.SSHConnection):
         if not promt_check:
             return -1
 
+        cleanedup_buffer = "".join([i if ord(i) < 128 else '?' for i in buff])
+
         # search possible promt
         for code in promt_check:
-            position = buff.find(code)
+            position = cleanedup_buffer.find(code)
             if position != -1:
                 return position
 
