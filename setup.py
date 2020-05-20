4a1f10d9e82bae9ca5fdb2ea2cc9a21d95a381c8
Mateusz Neumann <mateumann@gmail.com>
2020-05-20 15:49:00 +0200

    Version bump (#32)

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import setuptools
 
 setuptools.setup(
     name='cloudify-utilities-plugins-sdk',
-    version='0.0.22',
+    version='0.0.24',
     author='Cloudify Platform Ltd.',
     author_email='hello@cloudify.co',
     description='Utilities SDK for extending Cloudify',
