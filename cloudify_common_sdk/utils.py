1d58a9874a2b4a2c5a8da3d7690e5387a246da05
AdarShaked <adar@cloudify.co>
2020-10-07 15:07:31 +0300

    name fixes

--- cloudify_common_sdk/utils.py
+++ cloudify_common_sdk/utils.py
@@ -10,20 +10,20 @@ def get_deployment_dir(deployment_id):
       :param deployment_id:  deployment id from cloudify context.
       :return: Deployment directory path
     """
-    deployments_old_workdir = os.path.join('/opt', 'mgmtworker', 'work',
-                                           'deployments',
-                                           get_tenant_name(),
-                                           deployment_id)
+    deployments_old_dir = os.path.join('/opt', 'mgmtworker', 'work',
+                                       'deployments',
+                                       get_tenant_name(),
+                                       deployment_id)
 
-    deployments_new_workdir = os.path.join('/opt', 'manager',
-                                           'resources',
-                                           'deployments',
-                                           get_tenant_name(),
-                                           deployment_id)
+    deployments_new_dir = os.path.join('/opt', 'manager',
+                                       'resources',
+                                       'deployments',
+                                       get_tenant_name(),
+                                       deployment_id)
 
-    if os.path.isdir(deployments_new_workdir):
-        return deployments_new_workdir
-    elif os.path.isdir(deployments_old_workdir):
-        return deployments_old_workdir
+    if os.path.isdir(deployments_new_dir):
+        return deployments_new_dir
+    elif os.path.isdir(deployments_old_dir):
+        return deployments_old_dir
     else:
         raise NonRecoverableError("No deployment directory found!")
