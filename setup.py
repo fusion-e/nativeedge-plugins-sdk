7185ad0c2cd64016fc42ad1859fde813074a634c
Ahmad Musa <ahmad@cloudify.co>
2020-11-02 15:47:34 +0200

    RD-278-Fix-Terminal-Encoding

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import setuptools
 
 setuptools.setup(
     name='cloudify-utilities-plugins-sdk',
-    version='0.0.31',
+    version='0.0.33',
     author='Cloudify Platform Ltd.',
     author_email='hello@cloudify.co',
     description='Utilities SDK for extending Cloudify',
