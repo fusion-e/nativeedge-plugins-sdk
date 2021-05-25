8c9c5a12ff4c8e8d00aba85daeeb4a649d44044b
earthmant <trammell@cloudify.co>
2021-05-25 13:53:50 +0300

    preserve unzip permissions

--- cloudify_common_sdk/tests/test_resource_downloader.py
+++ cloudify_common_sdk/tests/test_resource_downloader.py
@@ -27,11 +27,14 @@ FILE_WITH_NO_TYPE_URL = \
     "https://codeload.github.com/cloudify-incubator/"\
     "cloudify-utilities-plugins-sdk/zip/master"
 
+os.system('sudo chmod -R /tmp 0770')
+
 
 class TestResourceDownloader(unittest.TestCase):
 
     def test_file_with_type(self):
-        result = resource_downloader.get_shared_resource(FILE_WITH_TYPE_URL)
+        result = resource_downloader.get_shared_resource(
+            FILE_WITH_TYPE_URL)
         self.assertTrue(os.path.exists(result))
 
     def test_file_with_no_type(self):
