5551ef1c469b1d5ec1ee857b7c19194162dbc6c1
EarthmanT <trammell@cloudify.co>
2021-09-30 12:34:12 -0400

    General Executor
    __NODOCS__

--- cloudify_common_sdk/ovf.py
+++ cloudify_common_sdk/ovf.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import xmltodict
-
 # Code based on:
 # https://github.com/virt-manager/virt-manager/blob/master/virtconv/ovf.py
 # Mapping of ResourceType value to device type
