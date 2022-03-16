ea0b57ce2b8509e44fad68fe4ca0dd3df1665cdb
EarthmanT <trammell@cloudify.co>
2022-03-16 13:28:16 -0400

    several improvements

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import setuptools
 
 setuptools.setup(
     name='cloudify-utilities-plugins-sdk',
-    version='0.0.59',
+    version='0.0.60',
     author='Cloudify Platform Ltd.',
     author_email='hello@cloudify.co',
     description='Utilities SDK for extending Cloudify',
