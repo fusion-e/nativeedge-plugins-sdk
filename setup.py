b59f3c57ac11a78f32377f4c272b3e8faf3a668e
EarthmanT <trammell@cloudify.co>
2022-03-30 10:46:10 -0400

    update logging info

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import setuptools
 
 setuptools.setup(
     name='cloudify-utilities-plugins-sdk',
-    version='0.0.61',
+    version='0.0.62',
     author='Cloudify Platform Ltd.',
     author_email='hello@cloudify.co',
     description='Utilities SDK for extending Cloudify',
