b07eb738b8de51c0d29d7e1d14379bf548ff8767
ahmadiesa-abu <ahmad@cloudify.co>
2022-05-20 00:06:44 +0300

    CYBL-1593-Handle-Dict-Obfuscation-Val

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import setuptools
 
 setuptools.setup(
     name='cloudify-utilities-plugins-sdk',
-    version='0.0.68',
+    version='0.0.69',
     author='Cloudify Platform Ltd.',
     author_email='hello@cloudify.co',
     description='Utilities SDK for extending Cloudify',
