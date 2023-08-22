9bb238f9181a36330da2e1487680e23ac2e0778c
Mateusz Mizerski <106737627+mateuszmizer@users.noreply.github.com>
2023-08-22 11:20:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setuptools.setup(
         'xmltodict',   # rest
         "gitdb>=0.6.4",  # shared download resource
         "GitPython",  # shared download resource
-        "cfy-lint>=0.0.31",
+        "cfy-lint>=0.0.23",
         'psutil',
         'packaging',
         'kubernetes==v26.1.0',
