2b2808c80c2471db671c5fa77695acc80959e6a9
EarthmanT <EarthmanT@users.noreply.github.com>
2024-05-09 15:03:11 -0400

    another level of debugging

--- nativeedge_common_sdk/utils.py
+++ nativeedge_common_sdk/utils.py
@@ -143,14 +143,19 @@ def get_blueprint_dir(blueprint_id=None):
     try:
         for file in os.walk(os.path.dirname(blueprint_dir)):
             ctx_from_import.logger.error(f'Walked file: {file}')
-    except Exception:
-        pass
+    except Exception as e:
+        ctx_from_import.logger.error(f'Exc: {e}')
     try:
         for file in os.walk(os.path.dirname(updated_blueprint_dir)):
             ctx_from_import.logger.error(f'BWalked file: {file}')
-    except Exception:
-        pass
-    
+    except Exception as e:
+        ctx_from_import.logger.error(f'2Exc: {e}')
+    try:
+        for file in os.walk(os.path.dirname(os.path.dirname(blueprint_dir))):
+            ctx_from_import.logger.error(f'CWalked file: {file}')
+    except Exception as e:
+        ctx_from_import.logger.error(f'3Exc: {e}')
+
     if os.path.isdir(blueprint_dir):
         return blueprint_dir
     elif os.path.isdir(updated_blueprint_dir):
