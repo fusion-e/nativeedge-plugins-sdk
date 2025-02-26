b8806a4eb6c4036ed35943b6b6506b305f75f800
earthmant <ttearthman@gmail.com>
2025-02-26 15:47:27 -0500

    [NE01D-189] verify sums

---
+++ nativeedge_common_sdk/tests/test_verify_hash.py
@@ -0,0 +1,17 @@
+import mock
+import unittest
+
+from nativeedge_common_sdk.verify_hashes import verify_hash
+
+
+class TestVerifyHash(unittest.TestCase):
+
+    @mock.patch('nativeedge_common_sdk.verify_hashes.generate_hash')
+    def test_verify_hash(self, mock_generate_hash):
+        mock_generate_hash.return_value = 'mock_generate_hash'
+        self.assertTrue(
+            verify_hash('file_name', 'mock_generate_hash file_name')
+        )
+        self.assertTrue(
+            verify_hash('file_name', 'mock_generate_hash')
+        )
