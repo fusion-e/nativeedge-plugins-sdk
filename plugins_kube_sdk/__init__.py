dc2153a6c838a6c147dedccd9ea281bad5330972
earthmant <ttearthman@gmail.com>
2025-03-23 15:35:33 -0400

    [NE01D-259] Rebrand for future compatibility.

---
+++ plugins_kube_sdk/__init__.py
@@ -0,0 +1,9 @@
+########
+# Copyright © 2025 Dell Inc. or its subsidiaries. All Rights Reserved.
+
+import sys
+
+import nativeedge_kubernetes_sdk
+
+
+sys.modules['plugins_kube_sdk'] = nativeedge_kubernetes_sdk
